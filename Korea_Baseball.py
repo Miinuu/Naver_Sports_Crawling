0d3ba28b3a2c48acdd685a3427a47f5ff55fcce0
Miinuu <113467403+Miinuu@users.noreply.github.com>
2022-10-06 22:55:30 +0900

    Add files via upload

--- Korea_Baseball.py
+++ Korea_Baseball.py
@@ -1,7 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime as dt
 
 #국내 야구 일정
+
+#일정 조회 함수
+def Schdeul_Search(search):
+    for key, item in matchData.items():
+        if (key == 'dateForSearch'):
+            if (item == search):
+                if (matchData.get('time') != '-'):
+                    ans = f"{matchData.get('date')} 일정은 {matchData.get('time')}에 홈 팀 {matchData.get('home')}, 원정 팀 {matchData.get('away')}(으)로 {matchData.get('stadium')}경기장에서 " \
+                             f"진행될 예정이며, {matchData.get('platform')}을 통해서 중계됩니다."
+                else:
+                    ans = "금일 경기 일정은 없습니다."
+    return ans
 
 months = [3,4,5,6,7,8,9,10]
 for month in months :
@@ -16,13 +29,19 @@ for month in months :
         for data in dataTb:
             #모든 날짜
             dateValue = data.find("span",{"class" : "td_date"}).text
+            #datetime에 맞게 정규화한 변수
+            dateValue2 = data.find("strong").text
+            dateValue2 = dateValue2.split(".")
+            dateValue2 = f"2022-{dateValue2[0]}-{dateValue2[1]}"
             if(len(dateValue.split(" ")[0].split(".")[1]) ==1):
+                #날짜 정규화
                 dateValue = dateValue.split(" ")[0].split(".")[0] + ".0" + dateValue.split(" ")[0].split(".")[1] + " " + dateValue.split(" ")[1]
             matchNum = data.find("td")["rowspan"]
             for i in range(int(matchNum)):
                 matchData = {} #모든 경기 정보 저장하는 딕셔너리
                 #날짜
                 matchData["date"] = dateValue
+                matchData["dateForSearch"] = dateValue2
                 #시간
                 matchData["time"] = data.findAll("tr")[i].find("span",{"class":"td_hour"}).text
                 #경기 없을 시 matchData["time"] = "-"
@@ -54,5 +73,7 @@ for month in months :
                 dataList.append(matchData)
 
     #데이터 정렬
-    result = sorted(dataList,key= lambda x: x["date"].split(" ")[0])
-    print(result)
+    #result = sorted(dataList,key= lambda x: x["date"].split(" ")[0])
+
+input = dt.date.today()
+Schdeul_Search(input)
